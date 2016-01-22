355b79dabd4986323803b674698e165db0291462
Aleksandr Bazhenov <Aleksandr Bazhenov>
2016-01-22 22:42:57 +0300

    I need more Div

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict' 
+
+var gulp=require("gulp"),
+    browserSync=require('browser-sync');
+gulp.task ('server', function () {
+    browserSync ({
+        port: 9000,
+        server: {
+            baseDir: 'app'
+        }
+    });
+});
+
+gulp.task ('watch', function () {
+    gulp.watch([
+        '*.html',
+        '*.js'
+        '*.css'
+    ]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);

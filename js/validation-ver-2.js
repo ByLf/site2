debb9715f1f643b88d0ca363a9c573a1c8bcdb11
Aleksandr Bazhenov <Aleksandr Bazhenov>
2016-02-11 19:17:12 +0300

    validation is complete

--- js/validation-ver-2.js
+++ js/validation-ver-2.js
@@ -1,99 +1,102 @@
 (function () {
     var doc = document,
         area = [],
-        data = [],
-        Num = 0,
-        tooltip_error = doc.getElementById('tooltip_Error').children;
-    function _validation(e) {
-        e.preventDefault();
-        console.dir(e);
-        _getNumderOfInput(e.target);
-        _stringDestroy();
-        var k = 0;
-        for (k = 0; k < Num; k++) {
-            _searchDangerousSymbol(data[k]);
-        }
-    }
-    function _getNumderOfInput(Form) {
-        var i = 0,
-            length = Form.length;
-        for (i = 0; i < length; i++) {
-            if (Form[i].tagName === 'INPUT') {
-                area[Num].form = Form[i];
-                area[Num].checkError = true;
-                area[Num].tooltip = tooltip_error[Num];
-                Num++;
+        Num,
+        tooltip_error = doc.getElementById('tooltip_Error').children,
+        _validation = function (e) {
+            e.preventDefault();
+            _getNumderOfInput(e.target);
+            var k = 0;
+            for (k = 0; k < Num; k++) {
+                area[k].checkError = _checkNullString(area[k].value);
+                if (area[k].checkError)
+                    { area[k].checkError = _searchDangerousSymbol(area[k].value); }
+                if (!area[k].checkError) {
+                    _showError(k);
+                    area[k].form.addEventListener('click', _clickErrorForm);
+                }
             }
-        }
-    }
-    function _searchDangerousSymbol(value) {
-        var i = 0,
-            inspect = true,
-            symbol = ['<', '>', '/*', '//'],
-            length = symbol.length;
-        for (i = 0; i < length; i++) {
-            if (value.indexOf(symbol[i]) >= 0) {
-                inspect = false;
-                break;
+        },
+        _getNumderOfInput = function (Form) {
+            var i = 0,
+                length = Form.length;
+            Num = 0;
+            for (i = 0; i < length; i++) {
+                if (Form[i].type === 'text' || Form[i].type === 'textarea') {
+                    area[Num] = {};
+                    area[Num].form = Form[i];
+                    area[Num].checkError = true;
+                    area[Num].tooltip = tooltip_error[Num];
+                    area[Num].value = Form[i].value;
+                    Num++;
+                }
             }
-        }
-        return inspect;
-    }
-    function _searchNeedingSymbol(value, symbols) {
-        var i = 0,
-            inspect = true,
-            length = symbols.length;
-        for (i = 0; i < length; i++) {
-            if (value.indexOf(symbols[i]) >= 0) {
-                inspect = false;
-                break;
+        },
+        _checkNullString = function (value) {
+            if (value.length < 1 || value == ' ') { return false; }
+                else {return true; }
+        },
+        _searchDangerousSymbol = function (value) {
+            var i = 0,
+                inspect = true,
+                symbol = ['<', '>', '/*', '//'],
+                length = symbol.length;
+            for (i = 0; i < length; i++) {
+                if (value.indexOf(symbol[i]) >= 0) {
+                    inspect = false;
+                    break;
+                }
             }
-        }
-        return inspect;
-    }
-    function _showError(Num) {
-        while (Num !== 0 && area[Num].tooltip == null) {
-            area[Num].tooltip = area[Num - 1].tooltip;
-        }
-        if (Num === 0 && area[Num].tooltip == null) { alert('Неверно введены данные!'); }
-        area[Num].checkError = false;
-        area[Num].tooltip
-            .classList.add('active');
-        area[Num].tooltip
-            .classList.remove('passive');
-        area[Num].form
-            .classList.add('Error');
-    }
-    function _hideError(Num) {
-        area[Num].checkError = true;
-        area[Num].tooltip
-            .classList.add('passive');
-        area[Num].tooltip
-            .classList.remove('active');
-        area[Num].form
-            .classList.remove('Error');
-    }
-    function _stringDestroy(value) {
-        var begin = 0,
-            end = 1,
-            length = value.length,
-            cash;
-        while (length > begin) {
-            end = value.indexOf('=');
-            cash = value.substring(begin, end);
-            begin = end + 1;
-            end = value.indexOf('&');
-            if (end < 0) { end = length; }
-            data[cash] = value.substring(begin, end);
-            begin = end + 1;
-        }
-    }
-    function _reset() {
-        var i = 0;
-        for (i = 0; i < Num; i++) {
-            if (area[i].checkError) {_hideError(i); }
-        }
-    }
+            return inspect;
+        },
+        _searchNeedingSymbol = function (value, symbols) {
+            var i = 0,
+                inspect = true,
+                length = symbols.length;
+            for (i = 0; i < length; i++) {
+                if (value.indexOf(symbols[i]) >= 0) {
+                    inspect = false;
+                    break;
+                }
+            }
+            return inspect;
+        },
+        _showError = function (Num) {
+            while (Num !== 0 && area[Num].tooltip == null) {
+                area[Num].tooltip = area[Num - 1].tooltip;
+            }
+            if (Num === 0 && area[Num].tooltip == null) { alert('Неверно введены данные!'); }
+            area[Num].checkError = false;
+            area[Num].tooltip
+                .classList.add('active');
+            area[Num].tooltip
+                .classList.remove('passive');
+            area[Num].form
+                .classList.add('Error');
+        },
+        _clickErrorForm = function (e) {
+            var i = 0,
+                form = e.target;
+            while (area[i].form !== form && i < Num) {
+                i++;
+            }
+            _hideError(i);
+        },
+        _hideError = function (Num) {
+            area[Num].checkError = true;
+            area[Num].tooltip
+                .classList.add('passive');
+            area[Num].tooltip
+                .classList.remove('active');
+            area[Num].form
+                .classList.remove('Error');
+        },
+        _reset = function () {
+            var i = 0;
+            for (i = 0; i < Num; i++) {
+                if (!area[i].checkError) {_hideError(i); }
+            }
+        };
     doc.addEventListener('submit', _validation);
     doc.addEventListener('reset', _reset);
 })();
